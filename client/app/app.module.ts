186d1a5b2c947a0d9f72d8486a482da9685fae36
Aswin Rajeev <aswinr.mg@gmail.com>
2019-03-03 22:26:38 +0530

    Added a toolbar to the UI

--- client/app/app.module.ts
+++ client/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent
   ],
   imports: [
     BrowserModule,
