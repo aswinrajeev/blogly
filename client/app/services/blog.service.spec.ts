62159e6c2ca5784d2f1ed28236bca32c8ed7843d
Aswin Rajeev <aswinr.mg@gmail.com>
2019-03-25 21:29:39 +0530

    Corrected an error with refractoring

--- client/app/services/blog.service.spec.ts
+++ client/app/services/blog.service.spec.ts
@@ -1,12 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 
-import { BlogdataService } from './blog.service';
+import { BlogService } from './blog.service';
 
 describe('BlogdataService', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
 
   it('should be created', () => {
-    const service: BlogdataService = TestBed.get(BlogdataService);
+    const service: BlogService = TestBed.get(BlogService);
     expect(service).toBeTruthy();
   });
 });
