2247dac4f82fbe5460e67335bbf3bacb3a401c75
Aswin Rajeev <aswinr.mg@gmail.com>
2019-10-20 17:48:04 +0530

    Some restructuring done for code cleanup.

--- server/app/blog_service.js
+++ server/app/blog_service.js
@@ -4,26 +4,24 @@ const { APIKeys } = require('../localconfigs/googleapi');
 const { BrowserWindow } =  require('electron');
 
 /**
- * 
+ * Handler for all blog related functionalities. 
+ * Single point of interface with UI for most of the UI backend communications.
  * @author Aswin Rajeev
  */
 class BlogService {
 
-	constructor(messenger, blogUrl) {
+	constructor(messenger, blogUrl, fileSystem) {
 		this.messenger = messenger;
 		this.blogUrl = blogUrl;
+		this.fs = fileSystem;
 	}
 
-	// initialize the state and listeners
+	/**
+	 * Initializes the blog adapter(s) and server-side listeners
+	 */
 	initialize() {
-		this.messenger.listen('publishblog', (data) => {
-			this.publishBlog(this.blogUrl, data.title, data.content, false, data.postId);
-		});
 
-		this.messenger.listen('publishdraft', (data) => {
-			this.publishBlog(this.blogUrl, data.title, data.content, true, data.postId);
-		});
-
+		// creates a new instance of Blogger Adapter.
 		this.blogger = new BloggerAdapter({
 			apiConf: new APIKeys(), 
 			appConf: {
@@ -32,16 +30,57 @@ class BlogService {
 			}, 
 			debugMode: false
 		});
+
+		this.initializeService();
 	}
 
-	// publish a blog
-	publishBlog(blogURL, title, contents, isDraft, postId) {
+	/**
+	 * Initializes the server-side listeners
+	 */
+	initializeService() {
+
+		// blog service related listeners
+		this.messenger.listen('publishblog', (data) => {
+			this.publishBlogPost(this.blogUrl, data._title, data._content, false, data._postId);
+		});
+		this.messenger.listen('publishdraft', (data) => {
+			this.publishBlogPost(this.blogUrl, data._title, data._content, true, data._postId);
+		});
+
+		// file system service relared listeners
+		this.messenger.respond('fetchposts', (data) => {
+			return this.fs.fetchPostsList();
+		});
+		this.messenger.respond('fetchFullPost', (data) => {
+			return this.fs.fetchPostData(data.filename);
+		});
+		this.messenger.respond('savePost', (data) => {
+			return this.fs.savePost(data.filename, data.postData);
+		})
+
+	}
+
+	/**
+	 * Publishes a post after authorizing with the user.
+	 * 
+	 * @param {*} blogURL - url of the blog
+	 * @param {*} title - title of the blog post
+	 * @param {*} contents - blog post contents
+	 * @param {*} isDraft - flag to specify if the post is to be published as draft
+	 * @param {*} postId - post id, if available.
+	 */
+	publishBlogPost(blogURL, title, contents, isDraft, postId) {
 		this.seekAuthorization(blogURL, (result) => {
-			this.uploadBlog(result.id, title, contents, isDraft, postId);
+			this.publishPostData(result.id, title, contents, isDraft, postId);
 		});
 	}
 
-	// open a new window for authorization
+	/**
+	 * Open a new window and redirect to Google auth service
+	 * 
+	 * @param {*} blogUrl - URL of the blog
+	 * @param {*} callback - callback function to be invoked after auth is completed
+	 */
 	seekAuthorization(blogUrl, callback) {
 		var window = new BrowserWindow({
 			height: 455,
@@ -54,15 +93,15 @@ class BlogService {
 			show: false
 		})
 		
-		// load the authorization URL
+		// loads the authorization URL
 		window.loadURL(this.blogger.generateAuthUrl());
 
 		window.once('ready-to-show', () => {
-			// open the window
+			// opens the window
 			window.show();
 		})
 		
-		// listen for an acknowledgement
+		// listens for an acknowledgement
 		const promise = this.blogger.awaitAuthorization({
 			blogAPI: this.blogger.getBloggerAPI(),
 			authClient: this.blogger.getAuth(),
@@ -81,8 +120,16 @@ class BlogService {
 		})
 	}
 
-	// publish the blog onto the blogger account
-	uploadBlog(blogId, title, contents, isDraft, postId) {
+	/**
+	 * Publishes the blog post, post auth
+	 * 
+	 * @param {*} blogId - id corresponding to the blog, as returned by the Google auth service
+	 * @param {*} title - title of the blog post
+	 * @param {*} contents - contents of the blog post
+	 * @param {*} isDraft - if to be saved as draft
+	 * @param {*} postId - post id for an existing blog post
+	 */
+	publishPostData(blogId, title, contents, isDraft, postId) {
 		this.blogger.publish({
 			blogAPI: this.blogger.getBloggerAPI(),
 			authClient: this.blogger.getAuth(),
@@ -97,8 +144,6 @@ class BlogService {
 			this.messenger.send('published', result);
 		})
 	}
-
-
 }
 
 module.exports.BlogService = BlogService;
--- server/app/files_service.js
+++ server/app/files_service.js
@@ -2,101 +2,168 @@ const fs = require('fs');
 const path = require('path');
 const h2p = require('html2plaintext');
 
+/**
+ * @author Aswin Rajeev
+ * 
+ * The file system service for blogly. All file system I/O operations are handled through this service.
+ */
 class FileSystemService {
 
 	constructor(app) {
+
+		this.BLOGLY_FILE_EXTN = "blogly";
+		this.INDEX_FILE_NAME = ".blogly.index";
+
+		// define the file system properties and initializes the required properties.
 		this.appDir = app.getPath('appData') + path.sep + "blogly"; //define the app dir
 		this.appConf = this.appDir  + path.sep +  "config.json";
-		this.blogsDir = app.getPath('documents') + path.sep + 'Blogly'; //set default blogs document dir
+
+		this.blogsDir = null;
 		this.conf = null;
-		this.indexFileName = ".blog.index";
+		this.indexFileName = this.INDEX_FILE_NAME;
 	}
 
-	// load the configurations for the application
+	/**
+	 * Initializes the filesystem service. This includes the following.
+	 * - reading the configurations from the app dir
+	 * - over-riding the default blog directory if defined
+	 *  */
 	initalize() {
 		this.conf = this.readConfigs();
 		this.blogsDir = this.conf.blogsDir;
 	}
 
-	// first-time initialize all pre-requisites
+	/**
+	 * Initializes the configuration for the first time. 
+	 */
 	initializeConfig() {
+
+		// initial window size
 		var conf = {
-			width: 1080, 
-			height: 640,
+			windowWidth: 1080, 
+			windowHeight: 640,
 		};
-		conf.blogsDir = this.blogsDir;
+
+		// initializes the blog directory as Blogly folder in documents.
+		conf.blogsDir = app.getPath('documents') + path.sep + 'Blogly';
 
 		try {
+
+			// creates the app directory if it does not exist
 			if (!fs.existsSync(this.appDir)) {
 				fs.mkdirSync(this.appDir);
 			}
+
+			//creates the blog directory
 			if (!fs.existsSync(this.blogsDir)){
 				fs.mkdirSync(this.blogsDir);
 			}
 	
 			// store the conf into a config file in the user app dir
 			fs.writeFileSync(this.appConf, JSON.stringify(conf), "utf8");
+
+			return conf;
 		} catch (error) {
-			console.error("Cannot create the configuration file.");
+			console.error("Could not create the configuration file. Please check if the application has sufficient permissions to read/write in the application data directory.", error);
+			throw error;
 		}
-
-
 	}
 
-	// returns the configuratins
+	/**
+	 * Get the index file path for the blog directory
+	 */
+	getIndexFile() {
+		return this.blogsDir + path.sep + this.indexFileName;
+	}
+
+	/**
+	 * Constructs and provide the fully qualified filepath from the filename
+	 * @param {*} filename - the file name for the file in question
+	 */
+	getFilePath(filename) {
+		return this.blogsDir + path.sep + filename;
+	}
+
+	/**
+	 * Reads the configuration from the app directory and returns it.
+	 */
 	readConfigs() {
+
 		try {
+			// reads the configurations for the app directory and returns the configuration
 			return JSON.parse(fs.readFileSync(this.appConf));
 		} catch (error) {
 			console.debug("Unable to load the conf file.")
 			try {
-
-				//do a first-time initialization
-				this.initializeConfig();
-				return JSON.parse(fs.readFileSync(this.appConf));
+				// do a first-time initialization, and returns the initial configuration
+				var conf = this.initializeConfig();
+				return conf;
 			} catch (error) {
-				console.error("Cannot create the configuration file.");
+				console.error("Cannot create the configuration file.", error);
 				throw error;
 			}
 		}
 	}
 
-	// save the configurations
+	/**
+	 * Writes the configuration into the filesystem.
+	 * @param {*} conf - configuration object to be saved.
+	 */
 	saveConfigs(conf) {
 		try {
 			fs.writeFileSync(this.appConf, JSON.stringify(conf), "utf8");
 		} catch (error) {
-			console.error("Cannot create the configuration file.");
+			console.error("Cannot create the configuration file.", error);
+			throw error;
 		}
 	}
 
-	// returns a property value
-	getProperty(name) {
+	/**
+	 * Reads a property from configuration by its key.
+	 * @param {*} key - key of the configuration property to read
+	 */
+	getConfigProperty(key) {
 		if (this.conf != null) {
-			return this.conf[name];
+			return this.conf[key];
 		} else {
 			return null;
 		}
 	}
 
-	// sets the messenger
-	setMessenger(messenger) {
-		this.messenger = messenger;
-		this.messenger.respond('fetchposts', (data) => {
-			return this.getPostsList();
-		});
+	/**
+	 * Sets a configuration property identified by key with the value.
+	 * Writes the configuration to the filesystem if writeConf is true.
+	 * @param {*} key - key of the configuration entry
+	 * @param {*} value - value for the configuration entry
+	 * @param {*} writeConf - specifies if the configuration has to be immediately written into the filesystem.
+	 */
+	setConfigProperty(key, value, writeConf) {
 
-		this.messenger.respond('fetchFullPost', (data) => {
-			return this.fetchPostData(data.filename);
-		});
+		// initializes the config if it doesn't exist
+		if (this.conf == null) {
+			this.conf = this.readConfigs();
+		}
 
-		this.messenger.respond('savePost', (data) => {
-			return this.savePost(data.filename, data.postData);
-		})
+		// sets the property corresponding to the key with value
+		this.conf[key] = value;
+
+		if (writeConf == true) {
+			this.saveConfigs(this.conf);
+		}
 	}
 
-	// creates / overwrites the indexing data
-	indexPostsFiles() {
+	/**
+	 * Sets the messenger object for the filesystem service. Messenger is used to provide log entries to the application from the filesystem service.
+	 * @param {*} messenger - the initialized messenger object
+	 */
+	setMessenger(messenger) {
+		this.messenger = messenger;
+	}
+
+	/**
+	 * Creates/over-writes an index for the current blogs directory.
+	 */
+	indexPosts() {
 		var blogs = [];
 		var blogIndex = {};
 		var count = 0;
@@ -105,23 +172,29 @@ class FileSystemService {
 		var currTime = Math.floor(Date.now());
 
 		try {
-			fs.readdirSync(this.blogsDir).forEach((file) => {
+
+			// read each file in the blogs directory.
+			fs.readdirSync(this.blogsDir).forEach( (file) => {
+
 				var filenameParts = file.split('.');
-				if (filenameParts[filenameParts.length - 1] == "post") {
+
+				// processes only if the file extension matches that of blogly
+				if (filenameParts[filenameParts.length - 1] == this.BLOGLY_FILE_EXTN) {
 	
 					try {
 						var blogPost = new Object();
 		
-						var post = JSON.parse(fs.readFileSync(this.blogsDir + path.sep + file, "utf8"));
+						// reads the contents of the file
+						var post = JSON.parse(fs.readFileSync(this.getFilePath(file), "utf8"));
 						
 						blogPost.title = post.title;
 						blogPost.postId = post.id;
-						blogPost.miniContent = h2p(post.content).slice(0, 100);
+						blogPost.miniContent = h2p(post.content).slice(0, 100); //updates only the mini-content. The original content is retrieved later.
 						blogPost.filename = file;
 		
+						// used for generation of index for the current blog post, and set it as the item id for the post
 						var timestamp = 'p_' + currTime;
 						currTime = currTime + 1;
-
 						blogPost.itemId = timestamp;
 		
 						blogIndex[timestamp] = blogPost;
@@ -129,7 +202,7 @@ class FileSystemService {
 		
 						count = count + 1;
 					} catch (error) {
-						console.debug('Error in reading the blog data');
+						console.error('Error in reading the blog data.', error);
 					}
 				}
 			});
@@ -138,27 +211,33 @@ class FileSystemService {
 			indexData.posts = blogs;
 			indexData.count = count;
 	
-			fs.writeFileSync(this.blogsDir + path.sep + this.indexFileName, JSON.stringify(indexData), "utf8");
+			// writes the index data into the index file
+			fs.writeFileSync(this.getIndexFile(), JSON.stringify(indexData), "utf8");
 		} catch (error) {
-			console.error('Error in indexing the posts.');
+			console.error('Error in indexing the posts.', error);
+			throw error;
 		}
 
 		return indexData;
 
 	}
 
-	// reads the post details from the index and returns the list
-	getPostsList() {
+	/**
+	 * Fetches the posts as specified in the current blogs index file.
+	 */
+	fetchPostsList() {
 		
 		var indexData = {};
 
 		try {
-			indexData = this.indexPostsFiles(); //TODO: Remove this
-			var content = fs.readFileSync(this.blogsDir + path.sep + this.indexFileName, "utf8");
+			indexData = this.indexPosts(); //TODO: Remove this
+
+			// reads content of the index file
+			var content = fs.readFileSync(this.getIndexFile(), "utf8");
 			indexData = JSON.parse(content);
 		} catch (error) {
 			console.error('Error in reading indices. Regenerating the indices...', error);
-			indexData = this.indexPostsFiles();
+			indexData = this.indexPosts();
 		}
 
 		var posts = [];
@@ -169,39 +248,58 @@ class FileSystemService {
 		return posts;
 	}
 
-	// reads the post file for a post and return the complete data
+	/**
+	 * Fetches the post data from the file provided.
+	 * @param {*} file - file path for the blogly post
+	 */
 	fetchPostData(file) {
 		var postData;
 
 		try {
-			var content = fs.readFileSync(this.blogsDir + path.sep + file, "utf8");
+
+			// read the content of the post file
+			var content = fs.readFileSync(this.getFilePath(file), "utf8");
 			postData = JSON.parse(content);
 
 			// add filename on to the data
 			postData.file = file;
 		} catch (error) {
 			console.error('Error in reading the post file.', error);
+			throw error;
 		}
 
 		return postData;
 	}
 
-	// writes the post data to the corresponding file
+	/**
+	 * Writes the post data into the file specified by filename.
+	 * @param {*} filename - filename for the file to which the post data is to be written
+	 * @param {*} post - the post data 
+	 */
 	savePost(filename, post) {
 
 		var postData = {};
-		var result = {status: 'ok'};
 		postData.content = post.content;
 		postData.title = post.title;
 		postData.itemId = post.itemId;
 		postData.postId = post.postId
 		postData.file = post.file;
 		postData.postURL = post.postURL;
-		fs.writeFileSync(this.blogsDir + path.sep + filename, JSON.stringify(postData));
 
-		// TODO: Update the cache also
+		if (filename == null) {
+			console.error('Filename is null.');
+			return null;
+		}
 
-		result.filename = filename;
+		try {
+			// writes the post data into the file
+			fs.writeFileSync(this.getFilePath(filename), JSON.stringify(postData));
+		} catch (error) {
+			console.error('Unable to write the post data to file.', error);
+		}
+
+		// TODO: Update the cache as well. Remove the next line to generate the full cache.
+		this.indexPosts();
 
 		return result;
 	}
